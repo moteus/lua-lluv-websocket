f081024e979e4d816c1528fa182cc10e23d315fe
Alexey Melnichuk <mimir@newmail.ru>
2015-02-27 10:51:33 +0400

    Add. Copyright comments.

--- src/lluv/websocket.lua
+++ src/lluv/websocket.lua
@@ -1,3 +1,15 @@
+------------------------------------------------------------------
+--
+--  Author: Alexey Melnichuk <alexeymelnichuck@gmail.com>
+--
+--  Copyright (C) 2015 Alexey Melnichuk <alexeymelnichuck@gmail.com>
+--
+--  Licensed according to the included 'LICENSE' document
+--
+--  This file is part of lua-lluv-ssl library.
+--
+------------------------------------------------------------------
+
 local uv        = require "lluv"
 local ut        = require "lluv.utils"
 local tools     = require "websocket.tools"
