53c97a2b8f2052857dac183261f8c188c2dafc64
Alexey Melnichuk <mimir@newmail.ru>
2015-03-05 15:08:52 +0400

    Use my fork of lua-websockets to test.

--- test/test.lua
+++ test/test.lua
@@ -4,3 +4,17 @@ local websocket  = require "websocket"
 assert(websocket.client.lluv)
 assert(websocket.client.lluv.sync)
 assert(websocket.server.lluv)
+
+local bit = require "websocket.bit"
+
+local ch = '\239'
+local m  = 0x0F
+local b  = string.byte(ch)
+local v  = bit.bxor(b, m)
+local a  = bit.band(v, 0xFF)
+print("BYTE:", b)
+print("MASK:", v)
+print("AND :", a)
+print("CAND:", string.char(a))
+print("CMSK:", string.char(v))
+
