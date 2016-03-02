a35f22ce01ea95a110e701b03f8d8a9bc7c7b8f3
Alexey Melnichuk <mimir@newmail.ru>
2016-03-02 16:30:36 +0300

    Add. LuaSocket API implementation.

---
+++ test/autobahn_co_client_test.lua
@@ -0,0 +1,97 @@
+local uv     = require "lluv"
+local ut     = require "lluv.utils"
+local socket = require "lluv.websocket.luasocket"
+
+local stp = require "StackTracePlus"
+
+local Autobahn  = require "./autobahn"
+
+local ctx do
+  local ok, ssl = pcall(require, "lluv.ssl")
+  if ok then
+    ctx = assert(ssl.context{
+      protocol    = "tlsv1",
+      certificate = "./wss/server.crt",
+    })
+  end
+end
+
+local Client = function() return socket.ws{ssl = ctx, utf8 = true} end
+
+local URI           = arg[1] or "ws://127.0.0.1:9001"
+local reportDir     = "./reports/clients"
+local agent         = "lluv.ws.luasocket"
+local caseCount     = 0
+local currentCaseId = 0
+
+local function getCaseCount(cont)
+  ut.corun(function()
+    local cli = Client()
+    local ok, err = cli:connect(Autobahn.Server.getCaseCount(URI))
+    if not ok then
+      cli:close()
+      return print("WS ERROR:", err)
+    end
+    local msg = cli:receive("*r")
+    caseCount = tonumber(msg)
+    cli:close()
+    cont()
+  end)
+end
+
+local function runTestCase(no, cb)
+  ut.corun(function()
+    local cli = Client()
+    local ok, err = cli:connect(Autobahn.Server.runTestCase(URI, no, agent))
+    if not ok then
+      cli:close()
+      return print("WS ERROR:", err)
+    end
+
+    print("Executing test case " .. no .. "/" .. caseCount)
+
+    while true do
+      local msg, opcode = cli:receive("*l")
+      if not msg then break end
+      if opcode == socket.TEXT or opcode == socket.BINARY then
+        cli:send(msg, opcode)
+      end
+    end
+    cli:close()
+    uv.defer(cb)
+  end)
+end
+
+local function updateReports()
+  ut.corun(function()
+    local cli = Client()
+    local ok, err = cli:connect(Autobahn.Server.updateReports(URI, agent))
+    if not ok then print("WS ERROR:", err) end
+    cli:close()
+  end)
+end
+
+local function runNextCase()
+  runTestCase(currentCaseId, function(_, err, code, reason)
+    currentCaseId = currentCaseId + 1
+    if currentCaseId <= caseCount then
+      runNextCase()
+    else
+      print("All test cases executed.")
+      updateReports()
+    end
+  end)
+end
+
+local function runAll()
+  currentCaseId = 1
+  Autobahn.cleanReports(reportDir)
+  getCaseCount(runNextCase)
+  uv.run(stp.stacktrace)
+
+  if not Autobahn.verifyReport(reportDir, agent, true) then
+    return os.exit(-1)
+  end
+end
+
+runAll()
